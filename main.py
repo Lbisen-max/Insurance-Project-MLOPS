0eaad8b61732b7ca1617ebb8871e4a8ec19dce50
Lbisen-max <lokeshbisen989@gmail.com>
2023-06-10 11:24:32 +0530

    data ingestion

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from Insurance.exceptions import InsuranceException
 import os 
 import sys
 from Insurance.utils import get_collection_as_dataframe
+from Insurance.entity.config_entity import DataIngestionConfig
+from Insurance.entity import config_entity
 
 # def test_logger_and_exception():
 #     try:
@@ -19,6 +21,9 @@ from Insurance.utils import get_collection_as_dataframe
 if __name__ == "__main__":
     try:
         #test_logger_and_exception()
-        get_collection_as_dataframe(database_name = "INSURANCE",collection_name ="INSURANCE_PROJECT")
+        #get_collection_as_dataframe(database_name = "INSURANCE",collection_name ="INSURANCE_PROJECT")
+        training_pipeline_config = config_entity.TraningPipelineConfig()
+        data_ingestion_config = config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+        print(data_ingestion_config.to_dict())
     except Exception as e:
         print(e)
